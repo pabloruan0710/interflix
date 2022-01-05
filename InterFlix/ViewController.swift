2244ff11230e62aa4112eb5a2d90081d4ba47024
Pablo Ruan 🧑🏼‍💻 <pablo.silva@bancointer.com.br>
2022-01-05 10:31:18 -0300

    Initial Commit

---
+++ InterFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterFlix
+//
+//  Created by Pablo Ruan Ribeiro Silva  on 05/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
